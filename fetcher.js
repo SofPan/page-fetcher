e00e9a20201d405dd786a68a142200a47a92236b
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-12 12:52:51 -0500

    add checks to ensure url and path are the correct arguments

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,14 @@
 const request = require('request');
 const userInput = process.argv.slice(2, 4);
+const url = userInput[0];
+const path = userInput[1];
 
+if (!url.includes("http")) {
+  console.log(`${url} is not the correct format. Please provide a complete URL.`);
+}
+
+if (path.includes("http")) {
+  console.log(`${path} must be a local path.`);
+}
 
 
