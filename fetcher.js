32ec0a14b320baf03f7529f200ae3e84cf95f50a
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-12 12:55:38 -0500

    send request to user specified url

--- fetcher.js
+++ fetcher.js
@@ -3,12 +3,16 @@ const userInput = process.argv.slice(2, 4);
 const url = userInput[0];
 const path = userInput[1];
 
-if (!url.includes("http")) {
+if (!url.includes("http") || !url) {
   console.log(`${url} is not the correct format. Please provide a complete URL.`);
 }
 
-if (path.includes("http")) {
+if (path.includes("http") || !path) {
   console.log(`${path} must be a local path.`);
 }
 
-
+request(url, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+});
